372fc8588b2a809d4d1a76ac69ec141e6547993f
Allan <altonallan47@gmail.com>
2026-02-16 16:59:13 +0300

    Update Library.tsx

--- components/Library.tsx
+++ components/Library.tsx
@@ -12,9 +12,7 @@ const Library = () => {
       <div className="flex items-center justify-between px-5 pt-4">
         <div className="inline-flex items-center gap-x-2">
           <TbPlaylist className="text-neutral-400" size={26} />
-          <p className="text-neutral-400 font-medium text-md">
-            Your Libarariaa
-          </p>
+          <p className="text-neutral-400 font-medium text-md">Your Libraria</p>
         </div>
         <AiOutlinePlus
           onClick={onClick}
