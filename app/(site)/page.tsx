c4f27c2ef199fc4298babeebb5c58717e0a54481
Allan <altonallan47@gmail.com>
2026-02-16 17:40:31 +0300

    Add a Play button

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -9,7 +9,11 @@ const page = () => {
         <div className="mb-2">
           <h1 className="text-white text-3xl font-semibold">Welcome bitch</h1>
           <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4">
-            <ListItem />
+            <ListItem
+              image="/images/liked.jpeg"
+              name="Liked songs"
+              href="liked"
+            />
           </div>
         </div>
       </Header>
