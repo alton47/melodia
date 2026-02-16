95613c8c72d6618a6dba9175a7db55477312c237
Allan <altonallan47@gmail.com>
2026-02-16 17:19:27 +0300

    Update page.tsx

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,4 +1,5 @@
 import Header from "@/components/Header";
+import ListItem from "@/components/ListItem";
 import React from "react";
 
 const page = () => {
@@ -7,7 +8,9 @@ const page = () => {
       <Header>
         <div className="mb-2">
           <h1 className="text-white text-3xl font-semibold">Welcome bitch</h1>
-          <div className="grid grid-cols-1 sm:grid-cols-2  xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4"></div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4">
+            <ListItem />
+          </div>
         </div>
       </Header>
     </div>
