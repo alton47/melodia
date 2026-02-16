19acf63e168274264eb16a0097fd8a7bc528a654
Allan <altonallan47@gmail.com>
2026-02-16 16:31:14 +0300

    Add Header for Mobile

---
+++ components/Header.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { BiSearch } from "react-icons/bi";
+import { HiHome } from "react-icons/hi";
+import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
+import { twMerge } from "tailwind-merge";
+
+interface HeaderProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+const Header: React.FC<HeaderProps> = ({ children, className }) => {
+  const router = useRouter();
+
+  const handleLogout = () => {
+    //sheitt
+  };
+
+  return (
+    <div
+      className={twMerge(
+        `h-fit bg-linear-to-b from-emerald-800 p-6`,
+        className,
+      )}
+    >
+      <div className="w-full mb-4 flex items-center justify-between">
+        <div className="hidden md:flex gap-x-2 items-center">
+          <button
+            className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition"
+            onClick={() => router.back()}
+          >
+            {" "}
+            <RxCaretLeft className="text-white" size={35} />
+          </button>
+          <button
+            className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition"
+            onClick={() => router.forward()}
+          >
+            {" "}
+            <RxCaretRight className="text-white" size={35} />
+          </button>
+        </div>
+        <div className="flex md:hidden gap-x-2 items-center">
+          <button className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition">
+            <HiHome className="text-black" size={20} />
+          </button>
+          <button className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition">
+            <BiSearch className="text-black" size={20} />
+          </button>
+        </div>
+        <div className="flex justify-between items-center gap-x-4">
+          <></>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
