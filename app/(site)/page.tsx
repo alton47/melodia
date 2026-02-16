8dcddc57e8d8883793d4e4428c6bce38dab12c49
Allan <altonallan47@gmail.com>
2026-02-16 17:48:53 +0300

    Updates

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -17,6 +17,12 @@ const page = () => {
           </div>
         </div>
       </Header>
+      <div className="mt-2 mb-7 px-6">
+        <div className="flex justify-between items-center">
+          <h1 className="text-white text-2xl font-semibold">Ngoma mpyaa</h1>
+        </div>
+        <div>List ya Ngomaa</div>
+      </div>
     </div>
   );
 };
