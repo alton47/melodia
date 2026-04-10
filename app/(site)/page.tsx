41d5fcea4bce1f035cb16f395b1af18494de220c
Allan <altonallan47@gmail.com>
2026-04-10 11:15:42 +0300

    chore: Clean slate

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -7,7 +7,7 @@ const page = () => {
     <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
       <Header>
         <div className="mb-2">
-          <h1 className="text-white text-3xl font-semibold">Welcome bitch</h1>
+          <h1 className="text-white text-3xl font-semibold">Welcome bitch..</h1>
           <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4">
             <ListItem
               image="/images/liked.jpeg"
